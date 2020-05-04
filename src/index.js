1d91b0a41351aca707fe74f8fdbb343095a19043
RajD <dike4mii4real@gmail.com>
2020-05-04 22:48:39 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { BrowserRouter as Router } from "react-router-dom";
 import history  from "./components/history";
 import { Provider } from 'react-redux';
 ReactDOM.render(
-<Provider store={store}>    
-<Router forceRefresh={true} history={history}>
-    <App /> 
-    </Router>
-</Provider>,document.getElementById('root'));
+    <Provider store={store}>    
+        <Router forceRefresh={true} history={history}>
+            <App /> 
+        </Router>
+    </Provider>,document.getElementById('root')
+);
 
