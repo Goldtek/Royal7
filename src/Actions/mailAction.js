1c7dba909d46cfdd8f152584f9afe3c5c89935db
RajD <dike4mii4real@gmail.com>
2020-05-04 23:14:58 +0100

    actions modified

--- src/Actions/mailAction.js
+++ src/Actions/mailAction.js
@@ -1,26 +1,38 @@
-import { SEND_MAIL, CONFIRM_MAIL, LOGIN_USER } from './type'
 import axios from 'axios';
+import { SEND_MAIL, CONFIRM_MAIL, LOGIN_USER } from './type'
 
-var api_url = process.env.REACT_APP_API_URL;
+const api_url = process.env.REACT_APP_API_URL;
 
 
 export const sendMailRequest = (data) => dispatch => {
+    try {
     return axios.post(`${api_url}/api/send/mail`, {email: data});
+    } catch (error) {
+        console.log(`error sending invitation ${error}`);
+    }
 }
 
 export const confirmMail_1 = data => dispatch => {
+    try{ 
     return axios.post(`${api_url}/api/admin/confirm`, {data});
+    } catch (error) {
+        console.log(`error confirming email ${error}`);
+    }
 }
 
 export const create_account = data => dispatch => {
-    return axios.post(`${api_url}/api/school/admin`, {
-        schoolName: data.schoolName,
-        phone: data.phone,
-        address: data.address,
-        email: data.email,
-        about: data.about,
-        password: data.password
-    })
+    try {
+        return axios.post(`${api_url}/api/school/admin`, {
+            schoolName: data.schoolName,
+            phone: data.phone,
+            address: data.address,
+            email: data.email,
+            about: data.about,
+            password: data.password
+        });
+    } catch (error) {
+        console.log(`error creating school account ${error}`);
+    }
 }
 
 export const loginUser = (data) => dispatch => {
