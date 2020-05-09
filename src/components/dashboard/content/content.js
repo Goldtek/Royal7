05d687a7014b202f95074dbf898c522a4930bd6d
Elemson Ifeanyi <ielemson>
2020-05-09 14:36:29 +0100

    split the dashboard into components

--- src/components/dashboard/content/content.js
+++ src/components/dashboard/content/content.js
@@ -2,58 +2,275 @@ import React from 'react'
 
 function content() {
     return (
-        <div class="st-pusher" id="content">
-                   
-                    <div class="st-content">
-                
-                        <div class="st-content-inner padding-none">
-                            <div class="container-fluid">
-                                <div class="page-section">
-                                    <h1 class="text-display-1">Dashboard</h1>
+
+
+        
+       
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-lg-12 clear-padding-xs">
+                <h5 className="page-title"><i className="fa fa-home"></i>HOME</h5>
+                <div className="section-divider"></div>
+                <div className="dashboard-stats">
+                    <div className="col-sm-6 col-md-3">
+                        <div className="stat-item">
+                            <div className="stats">
+                                <div className="col-xs-8 count">
+                                    <h1>199</h1>
+                                    <p>STUDENTS</p>
                                 </div>
-                                <div class="row" data-toggle="isotope">
-                                    <div class="item col-xs-12 col-lg-6">
-                                        <div class="panel panel-default paper-shadow" data-z="0.5">
-                                            <div class="panel-heading">
-                                                <div class="media v-middle">
-                                                    <div class="media-body">
-                                                        <h4 class="text-headline margin-none">Earnings</h4>
-                                                        <p class="text-subhead text-light">This Month</p>
-                                                    </div>
-                                                    <div class="media-right">
-                                                        <a class="btn btn-white btn-flat" href="app-instructor-earnings.html">Reports</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="panel-body">
-                                                <div id="line-holder" data-toggle="flot-chart-earnings" class="flotchart-holder height-200"></div>
-                                            </div>
-                                            <hr/>
-                                            <div class="panel-body">
-                                                <div class="row text-center">
-                                                    <div class="col-md-6">
-                                                        <h4 class="margin-none">Gross Revenue</h4>
-                                                        <p class="text-display-1 text-warning margin-none">102.4k</p>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <h4 class="margin-none">Net Revenue</h4>
-                                                        <p class="text-display-1 text-success margin-none">55k</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                <div className="col-xs-4 icon">
+                                    <i className="fa fa-users ex-icon"></i>
+                                </div>
+                                <div className="clearfix"></div>
+                            </div>
+                            <div className="status">
+                                <p className="text-ex"><i className="fa fa-pencil-square-o"></i>10 Absent Today</p>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <div className="stat-item">
+                            <div className="stats">
+                                <div className="col-xs-8 count">
+                                    <h1>111</h1>
+                                    <p>TEACHERS</p>
+                                </div>
+                                <div className="col-xs-4 icon">
+                                    <i className="fa fa-user-secret danger-icon"></i>
+                                </div>
+                                <div className="clearfix"></div>
+                            </div>
+                            <div className="status">
+                                <p className="text-danger"><i className="fa fa-exclamation-triangle"></i>5 On Leave Today</p>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>
+                    <div className="clearfix visible-sm"></div>
+                    <div className="col-sm-6 col-md-3">
+                        <div className="stat-item">
+                            <div className="stats">
+                                <div className="col-xs-8 count">
+                                    <h1>900</h1>
+                                    <p>EVENTS</p>
+                                </div>
+                                <div className="col-xs-4 icon">
+                                    <i className="fa fa-flag look-icon"></i>
+                                </div>
+                                <div className="clearfix"></div>
+                            </div>
+                            <div className="status">
+                                <p className="text-look"><i className="fa fa-clock-o"></i>1 Event tomorrow</p>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 col-md-3">
+                        <div className="stat-item">
+                            <div className="stats">
+                                <div className="col-xs-8 count">
+                                    <h1>799</h1>
+                                    <p>MESSAGES</p>
+                                </div>
+                                <div className="col-xs-4 icon">
+                                    <i className="fa fa-envelope-o success-icon"></i>
+                                </div>
+                                <div className="clearfix"></div>
+                            </div>
+                            <div className="status">
+                                <p className="text-success"><i className="fa fa-folder-open-o"></i>10 Unread messages</p>
+                            </div>
+                            <div className="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-12 clear-padding-xs">
+                <div className="col-sm-8">
+                    <div>
+                        <div className="my-msg dash-item">
+                            <h6 className="item-title"><i className="fa fa-bar-chart"></i>STUDENT ATTENDENCE TREND</h6>
+                            <div className="inner-item">
+                                <div className="summary-chart">
+                                    <canvas id="studentAttendenceLine" height="100px"></canvas>
+                                    <div className="chart-legends">
+                                        <span className="red">ABSENT</span>
+                                        <span className="orange">ON LEAVE</span>
+                                        <span className="green">PRESENT</span>
                                     </div>
-                                   </div>
+                                    <div className="chart-title">
+                                        <h6 className="bottom-title">STUDENT ATTENDENCE TREND</h6>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                   
                     </div>
-                 
-                
-                <footer class="footer">
-                    <strong>Learning</strong> v1.0.0 &copy; Copyright 2015
-                </footer>
                 </div>
+                <div className="col-sm-4">
+                    <div>
+                        <div className="my-msg dash-item">
+                            <h6 className="item-title"><i className="fa fa-calendar"></i>TODAY'S TASK</h6>
+                            <div className="inner-item">
+                                <div className="timetable-item">
+                                    <div className="col-xs-3 clear-padding">
+                                        <p><span className="time">10 AM</span></p>
+                                    </div>
+                                    <div className="col-xs-9">
+                                        <p className="title">Teacher Meeting</p>
+                                        <p className="sent-by"><i className="fa fa-map-marker"></i> ROOM NO - 601</p>
+                                    </div>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <div className="timetable-item">
+                                    <div className="col-xs-3 clear-padding">
+                                        <p><span className="time">11 AM</span></p>
+                                    </div>
+                                    <div className="col-xs-9">
+                                        <p className="title">Campus Tour</p>
+                                        <p className="sent-by"><i className="fa fa-map-marker"></i> CAMPUS</p>
+                                        
+                                    </div>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <div className="timetable-item">
+                                    <div className="col-xs-3 clear-padding">
+                                        <p><span className="time">12 PM</span></p>
+                                    </div>
+                                    <div className="col-xs-9">
+                                        <p className="title">Parent Meeting</p>
+                                        <p className="sent-by"><i className="fa fa-map-marker"></i> ROOM NO - 601</p>
+                                        
+                                    </div>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <div className="timetable-item">
+                                    <div className="col-xs-3 clear-padding">
+                                        <p><span className="time">01 PM</span></p>
+                                    </div>
+                                    <div className="col-xs-9">
+                                        <p className="title">Guest Lecture</p>
+                                        <p className="sent-by"><i className="fa fa-map-marker"></i> ROOM NO - 601</p>
+                                        
+                                    </div>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <div className="timetable-item">
+                                    <div className="col-xs-3 clear-padding">
+                                        <p><span className="time">02 PM</span></p>
+                                    </div>
+                                    <div className="col-xs-9">
+                                        <p className="title">Teacher Meeting</p>
+                                        <p className="sent-by"><i className="fa fa-map-marker"></i> ROOM NO - 601</p>
+                                    </div>
+                                    <div className="clearfix"></div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-12 clear-padding-xs">
+                <div className="col-sm-8">
+                    <div>
+                        <div className="my-msg dash-item">
+                            <h6 className="item-title"><i className="fa fa-bar-chart"></i>TODAY'S STUDENT ATTENDENCE</h6>
+                            <div className="inner-item">
+                                <div className="summary-chart">
+                                    <canvas id="studentAttendenceBar" height="100px"></canvas>
+                                    <div className="chart-legends">
+                                        <span className="red">ABSENT</span>
+                                        <span className="orange">ON LEAVE</span>
+                                        <span className="green">PRESENT</span>
+                                    </div>
+                                    <div className="chart-title">
+                                        <h6 className="bottom-title">STUDENT ATTENDENCE BAR</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div>
+                        <div className="my-msg dash-item">
+                            <h6 className="item-title"><i className="fa fa-pie-chart"></i>TEACHER ATTENDENCE</h6>
+                            <div className="chart-item">
+                                <canvas id="studentPie" style={{height:"250px"}}></canvas>
+                                <div className="chart-legends">
+                                    <span className="red">ABSENT</span>
+                                    <span className="orange">PRESENT</span>
+                                    <span className="green">LEAVE</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-12 clear-padding-xs">
+                <div className="col-md-12">
+                    <div className="my-msg dash-item">
+                        <h6 className="item-title"><i className="fa fa-bullhorn"></i>ANNOUNCEMENTS</h6>
+                        <div className="inner-item dashboard-tabs">
+                            <ul className="nav nav-tabs">
+                                <li className="active">
+                                    <a  href="#1" data-toggle="tab"><i className="fa fa-graduation-cap"></i><span>ACADEMICS</span></a>
+                                </li>
+                                <li>								
+                                    <a href="#2" data-toggle="tab"><i className="fa fa-users"></i><span>ADMISSIONS</span></a>
+                                </li>
+                                <li>								
+                                    <a href="#3" data-toggle="tab"><i className="fa fa-trophy"></i><span>SPORTS</span></a>
+                                </li>
+                            </ul>
+                            <div className="tab-content">
+                                <div className="tab-pane active" id="1">
+                                    <div className="announcement-item">
+                                        <h5>Guest lecture on fine arts by Smith.<span className="new">New</span></h5>
+                                        <h6><i className="fa fa-clock-o"></i>06-24-2017, 13:34</h6>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                        <div className="posted-by">
+                                            <p>Thanks,</p>
+                                            <h6>John Doe, Principal</h6>
+                                        </div>
+                                    </div>
+                                    <div className="announcement-item">
+                                        <h5>Guest lecture on fine arts by Smith</h5>
+                                        <h6><i className="fa fa-clock-o"></i>06-24-2017, 13:34</h6>
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+                                        <div className="posted-by">
+                                            <p>Thanks,</p>
+                                            <h6>John Doe, Principal</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="tab-pane" id="2">
+                                    <div className="announcement-item">
+                                        <h5>2</h5>
+                                    </div>
+                                </div>
+                                <div className="tab-pane" id="3">
+                                    <div className="announcement-item">
+                                        <h5>3</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
     )
 }
 
--- src/components/dashboard/sidebar/sidebar.js
+++ src/components/dashboard/sidebar/sidebar.js
@@ -3,51 +3,111 @@ import React, { Fragment } from 'react'
 const sidebar=()=> {
     return (
       <Fragment>
-           <div class="sidebar left sidebar-size-3 sidebar-offset-0 sidebar-visible-desktop sidebar-visible-mobile sidebar-skin-dark" id="sidebar-menu" data-type="collapse">
-            <div data-scrollable>
-                <div class="sidebar-block">
-                    <div class="profile">
-                        <a href="#">
-                            <img src="images/people/110/guy-6.jpg" alt="people" class="img-circle width-80" />
-                        </a>
-                        <h4 class="text-display-1 margin-none">Student Name</h4>
-                    </div>
-                </div>
-                <ul class="sidebar-menu">
-                    <li><a href="app-student-dashboard.html"><i class="fa fa-bar-chart-o"></i><span>Dashboard</span></a></li>
-                    <li class="hasSubmenu">
-                        <a href="#course-menu"><i class="fa fa-mortar-board"></i><span>Courses</span></a>
-                        <ul id="course-menu">
-                            <li><a href="app-directory-grid.html"><span>Courses Grid</span></a></li>
-                            <li><a href="app-directory-list.html"><span>Courses List</span></a></li>
-                            <li><a href="app-student-course.html"><span>Course Page</span></a></li>
-                            <li><a href="app-student-courses.html"><span>My Courses</span></a></li>
-                            <li><a href="app-take-course.html"><span>Take Course</span></a></li>
-                            <li><a href="app-course-forums.html"><span>Course Forums</span></a></li>
-                            <li><a href="app-course-forum-thread.html"><span>Course Forum Thread</span></a></li>
-                            <li><a href="app-take-quiz.html"><span>Take Quiz</span></a></li>
-                        </ul>
-                    </li>
-                    <li class="hasSubmenu active open">
-                        <a href="#forum-menu"><i class="fa fa-file-text-o"></i><span>Forum</span></a>
-                        <ul id="forum-menu" class="in">
-                            <li><a href="app-forum.html"><span>Forum Home</span></a></li>
-                            <li class="active"><a href="app-forum-category.html"><span>Forum Category</span></a></li>
-                            <li><a href="app-forum-thread.html"><span>Forum Thread</span></a></li>
-                        </ul>
-                    </li>
-                    <li class="hasSubmenu">
-                        <a href="#account-menu"><i class="fa fa-user"></i><span>Account</span></a>
-                        <ul id="account-menu">
-                            <li><a href="app-student-profile.html"><span>Edit Profile</span></a></li>
-                            <li><a href="app-student-billing.html"><span>Edit Billing</span></a></li>
-                        </ul>
-                    </li>
-                    <li><a href="app-student-messages.html"><i class="fa fa-comments"></i><span>Messages</span></a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out"></i><span>Logout</span></a></li>
-                </ul>
-            </div>
-        </div>
+        	<div className="sidebar-nav-wrapper" id="sidebar-wrapper">
+				<ul className="sidebar-nav">
+					<li>
+						<a href="admin-dashboard.html"><i className="fa fa-home menu-icon"></i> HOME</a>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-users menu-icon"></i> STUDENTS <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="admin-add-student.html"><i className="fa fa-caret-right"></i>ADD</a>
+							</li>
+							<li>
+								<a href="admin-student-list.html"><i className="fa fa-caret-right"></i>ALL STUDENT  </a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-user-secret menu-icon"></i> TEACHERS <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="admin-add-teacher.html"><i className="fa fa-caret-right"></i>ADD</a>
+							</li>
+							<li>
+								<a href="admin-teacher-list.html"><i className="fa fa-caret-right"></i>ALL TEACHER</a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+					<li>
+						<a href="message.html"><i className="fa fa-envelope menu-icon"></i> MY MESSAGES</a>
+					</li>
+					<li>
+						<a href="admin-add-announcement.html"><i className="fa fa-bullhorn menu-icon"></i> ANNOUNCEMENTS</a>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-file-o menu-icon"></i> CLASSES <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="admin-add-class.html"><i className="fa fa-caret-right"></i>ADD CLASS</a>
+							</li>
+							<li>
+								<a href="admin-add-section.html"><i className="fa fa-caret-right"></i>ADD SECTION</a>
+							</li>
+							<li>
+								<a href="admin-add-class.html"><i className="fa fa-caret-right"></i>VIEW SECTIONS</a>
+							</li>
+							<li>
+								<a href="admin-add-section.html"><i className="fa fa-caret-right"></i>VIEW CLASSES</a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-book menu-icon"></i> SUBJECTS <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="admin-add-subject.html"><i className="fa fa-caret-right"></i>ADD</a>
+							</li>
+							<li>
+								<a href="admin-add-subject.html"><i className="fa fa-caret-right"></i>VIEW SUBJECTS</a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-calendar menu-icon"></i> TIMETABLE <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="admin-create-timetable.html"><i className="fa fa-caret-right"></i>CREATE</a>
+							</li>
+							<li>
+								<a href="admin-class-timetable.html"><i className="fa fa-caret-right"></i>VIEW</a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown">
+							<i className="fa fa-address-card menu-icon"></i> REPORTS <span className="caret"></span>
+						</a>
+						<ul className="dropdown-menu">
+							<li>							
+								<a href="teacher-attendence-report.html"><i className="fa fa-caret-right"></i>ATTENDENCE</a>
+							</li>
+							<li>
+								<a href="teacher-marks-report.html"><i className="fa fa-caret-right"></i>MARKS</a>
+							</li>
+						</ul>
+						<div className="clearfix"></div>
+					</li>
+				</ul>
+			</div>
+			
+	
       </Fragment>
     )
 }
