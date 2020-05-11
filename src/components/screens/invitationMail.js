110d4273f4c8a78272089f46ddb6a7120d045bb9
Elemson Ifeanyi <ielemson>
2020-05-11 11:42:12 +0100

    rectified the create page and navabr

--- src/components/screens/invitationMail.js
+++ src/components/screens/invitationMail.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import axios from 'axios';
+import axios from "axios";
 import history from "../history";
 // import FooterComponent from "../footer/footer";
 // import HeaderComponent from "../header/navbar";
@@ -22,11 +22,11 @@ class Mail extends Component {
 
     sendMail = (email) => {
         try {
-            return axios.post(`${api_url}/api/send/mail`, {email});
-            } catch (error) {
-                console.log(`error sending invitation ${error}`);
-            }
-    }
+            return axios.post(`${api_url}/api/send/mail`, { email });
+        } catch (error) {
+            console.log(`error sending invitation ${error}`);
+        }
+    };
 
     onChange = (e) => {
         this.setState({ [e.target.name]: e.target.value });
@@ -62,12 +62,14 @@ class Mail extends Component {
     render() {
         const { errors } = this.state;
         return (
-            <div
-                className="container-fluid main-wrapper"
-                style={{ overflow: "hidden", width: "100%", padding: 0 }}
-            >
-                <div className="row">
-                    <div className="col-sm-12 col-xs-12 col-lg-4 col-md-4 login-section-wrapper">
+            <div className="container-fluid" style={{
+                overflow: "hidden",
+                width: "100%",
+                padding: 0,
+               
+            }}>
+                <div className="row" >
+                    <div className="col-sm-12 col-xs-12 col-lg-4 col-md-4" >
                         <div className="brand-wrapper">
                             <img
                                 src="images/logo/logo.png"
@@ -108,31 +110,18 @@ class Mail extends Component {
                                         </span>
                                     )}
                                 </div>
-
-                                {/* <div className='form-group'>
-<div className="form-check">
-<label className="form-check-label">
-<input 
-type="checkbox"
-className="form-check-input"
-checked={this.state.checked}
-onChange={this.checkboxHandler}
-/> It's okay to send me update emails.
-</label>
-</div>
-</div> */}
                                 <button
-                                    className="btn btn-md login-btn center-block"
+                                    className="btn btn-lg login-btn center-block"
                                     onClick={this.sendMail}
                                     type="button"
                                 >
                                     Next <i className="fa fa-arrow-right"></i>
                                 </button>
-                                {/* <input name="login" id="login" className="btn btn-block login-btn" type="button" value="Login"/> */}
+                           
                             </form>
                         </div>
                     </div>
-                    <div className="col-sm-6 col-xs-6 col-lg-8 col-md-8 half-div">
+                    <div className="col-sm-12 col-xs-12 col-lg-8 col-md-8" style={{padding:0,overflow:"hidden"}}>
                         <img
                             src="images/screens/education.png"
                             alt="login_image"
