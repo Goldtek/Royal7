d3f8b14a873216e5b92acb40345509f3b3d6acb2
Elemson Ifeanyi <ielemson>
2020-06-07 23:08:58 +0100

    sceen 1 email admin email account creation

--- src/components/screens/emailSent.js
+++ src/components/screens/emailSent.js
@@ -1,37 +1,42 @@
-import React, { Fragment } from 'react';
-const emailSent=()=> 
-(
-<Fragment>
-<div class="context">
-<h1>Check your email</h1>
-<div className=" card col-xs-12 col-md-4 col-lg-4 col-sm-4 col-md-offset-4 sent-div">
-<img src="images/screens/email-sent.png" alt=""/>
-<p>
-Check your <b>ielemson@gmail.com</b> inbox for instructions <br/> on how to confirm your account.
-{/* <a href="/" onClick={()=>{console.log("clicked")}}>Go back to login screen</a> */}
-</p>
+import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
 
-</div>
+function EmailSent() {
+    // Get the router object
+    const params = useParams();
 
-</div>
+    return (
+        <Fragment>
+            <div class="context">
+                <h1>Check your email</h1>
+                <div className=" card col-xs-12 col-md-4 col-lg-4 col-sm-4 col-md-offset-4 sent-div">
+                    <img
+                        src="images/screens/email-sent.png"
+                        alt={params.email}
+                    />
+                    <p>
+                        Check your <b>{params.email}</b> inbox for instructions{" "}
+                        <br /> on how to confirm your account.
+                    </p>
+                </div>
+            </div>
 
+            <div class="area">
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </Fragment>
+    );
+}
 
-<div class="area" >
-<ul class="circles">
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-</ul>
-</div >
-</Fragment>
-)
-
-
-export default emailSent
+export default EmailSent;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Route from "./routes";
 // import store from './stores';
 import { BrowserRouter as Router } from "react-router-dom";
-import history  from "./components/history";
-import { Provider } from 'react-redux';
+import history from "./components/history";
+// import { Provider } from 'react-redux';
 ReactDOM.render(
-    // <Provider store={store}>    
-        <Router forceRefresh={true} history={history}>
-            <App /> 
-        </Router>
+    // <Provider store={store}>
+    <Router forceRefresh={true} history={history}>
+        <Route />
+    </Router>,
     // </Provider>
-    ,document.getElementById('root')
+    document.getElementById("root")
 );
-
