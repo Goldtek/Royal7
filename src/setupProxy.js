c9d8b671b1b5d228713253e3932e18b5f11f2a86
Elemson Ifeanyi <ielemson>
2020-06-08 21:24:53 +0100

    removed process.env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const api_url = process.env.REACT_APP_BASE_URL;
+// const api_url = process.env.REACT_APP_BASE_URL;
 module.exports = function (app) {
     app.use(
         createProxyMiddleware("/api/send/mail", {
-            target: `${api_url}`,
+            target: `https://royal7.herokuapp.com`,
             changeOrigin: true,
         })
     );
